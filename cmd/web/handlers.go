8febb3beaf9ec2aef0d8240cb939f88a9a3c1d21
Sachin0P <sachinprakashramesh@gmail.com>
2026-06-14 21:41:17 +0530

    get

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
+	"snippetbox-m0ta-b1lla/internal/models"
 	"strconv"
 )
 
@@ -30,8 +32,15 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			http.NotFound(w, r)
+		} else {
+			app.serverError(w, r, err)
+		}
+	}
+	fmt.Fprintf(w, "%+v", snippet)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -39,6 +48,12 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte("Save a new snippet..."))
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, r, err)
+	}
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,30 +6,34 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"snippetbox-m0ta-b1lla/internal/models"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
-	logger *slog.Logger
+	logger   *slog.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-	app := &application{
-		logger: logger,
-	}
-	addr := flag.String("addr", ":4000", "http port value")
 	dsn := flag.String("dsn", "web:2401@/snippetbox?parseTime=true", "entry point of sql database")
+	addr := flag.String("addr", ":4000", "http port value")
 	flag.Parse()
-
-	logger.Info("starting server", "addr", *addr)
 	db, err := OpenDB(*dsn)
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
+	app := &application{
+		logger:   logger,
+		snippets: &models.SnippetModel{DB: db},
+	}
+
+	logger.Info("starting server", "addr", *addr)
+
 	defer db.Close()
 	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
